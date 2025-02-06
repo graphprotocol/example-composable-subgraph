7032462e4426e7558b067394e70dbaaa8c2f5fbf
Ion Suman <47307091+isum@users.noreply.github.com>
2025-02-06 13:39:23 +0200

    feat: add block-time subgraph

---
+++ block-time-subgraph/src/mapping.ts
@@ -0,0 +1,19 @@
+import { ethereum, BigInt } from "@graphprotocol/graph-ts";
+import { BlockTime } from "../generated/schema";
+
+export function handleBlock(block: ethereum.Block): void {
+  let blockTime = new BlockTime(block.hash.toHex());
+
+  blockTime.parentId = block.parentHash.toHex();
+  blockTime.timestamp = block.timestamp;
+
+  let parentBlockTime = BlockTime.load(block.parentHash.toHex());
+
+  if (parentBlockTime) {
+    blockTime.blockTime = blockTime.timestamp.minus(parentBlockTime.timestamp);
+  } else {
+    blockTime.blockTime = BigInt.fromU64(0);
+  }
+
+  blockTime.save();
+}
